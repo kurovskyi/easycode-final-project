8e6f0fc07fd68bf85d255402ce261a93f921c40a
CarbonyDK <dmytro.kurovskyi@gmail.com>
2019-11-08 18:18:52 +0200

    Starter files

--- src/config/apiConfig.js
+++
@@ -1,7 +0,0 @@
-const api = {
-  key: 'b991256',
-  url: 'http://www.omdbapi.com/',
-  file: '../data/imdb_top250.json',
-};
-
-export default api;
--- src/config/mainConfig.js
+++
@@ -1,5 +0,0 @@
-const main = {
-  moviesOnPage: 12,
-};
-
-export default main;
